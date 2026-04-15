2435d8fe6cb8318aceacf3c1c3bf347b30d193a0
Alessandro Lo Verde <alessandro.loverde@gmail.com>
2026-04-15 12:21:02 +0200

    ✓ Test background in svg - Not optimized, 574kb, minor problems with terrain accet gradient and clouds drop-shadow

--- pages/index.vue
+++ pages/index.vue
@@ -47,22 +47,30 @@ const ui = computed(() => {
 </script>
 
 <template>
-  <div class="p-4 text-center">
-    <div v-if="store.loading">Loading...</div>
+  <div class="page-bg flex min-h-screen items-center justify-center p-4 text-center text-white">
+    <div>
+      <div v-if="store.loading">Loading...</div>
 
-    <div v-else-if="store.error">Error: {{ store.error }}</div>
+      <div v-else-if="store.error">Error: {{ store.error }}</div>
 
-    <div v-else-if="store.weather">
-      <h1 class="text-6xl font-bold">{{ store.weather.temperature }}°</h1>
-      <p class="mt-2 text-lg">Weather code: {{ store.weather.weathercode }}</p>
-    </div>
+      <div v-else-if="store.weather">
+        <h1 class="text-6xl font-bold">{{ store.weather.temperature }}°</h1>
+        <p class="mt-2 text-lg">Weather code: {{ store.weather.weathercode }}</p>
+      </div>
 
-    <div v-if="ui">
-      <p class="mt-4 text-sm">{{ ui.key }}</p>
-    </div>
+      <div v-if="ui">
+        <p class="mt-4 text-sm">{{ ui.key }}</p>
+      </div>
 
-    <div v-if="store.city">
-      <p class="mt-4 text-sm">{{ store.city }} - {{ store.country }}</p>
+      <div v-if="store.city">
+        <p class="mt-4 text-sm">{{ store.city }} - {{ store.country }}</p>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.page-bg {
+  background: url('~/assets/images/PartlyCloudy -9.svg') top center / 100% auto no-repeat;
+}
+</style>
